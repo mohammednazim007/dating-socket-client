c1eb30da7d86f98edad73fe2b777ddb56b94da79
mohammednazim007 <mohammednazim3629@gmail.com>
2025-09-21 19:49:08 +0600

    feat: add profile name

--- src/app/components/ui/User-profile.tsx
+++ src/app/components/ui/User-profile.tsx
@@ -11,24 +11,32 @@ const UserProfile = ({ currentUser, isDisable }: UserProfileProps) => {
   const router = useRouter();
 
   return (
-    <button
-      disabled={isDisable}
+    <div
       onClick={() => router.push("/profile")}
-      className={`w-10 h-10 rounded-full overflow-hidden border-2 border-white flex items-center justify-center 
+      className="flex justify-center gap-1 cursor-pointer"
+    >
+      <button
+        disabled={isDisable}
+        className={`w-10 h-10 rounded-full overflow-hidden border-2 border-white flex items-center justify-center 
       ${
         isDisable
           ? "bg-gray-400 cursor-not-allowed pointer-events-none"
           : "bg-blue-600 hover:bg-blue-700"
       }`}
-    >
-      <Image
-        width={40}
-        height={40}
-        src={currentUser?.image || avatar.src}
-        alt={currentUser?.name || "User Avatar"}
-        className="w-full h-full object-cover"
-      />
-    </button>
+      >
+        <Image
+          width={40}
+          height={40}
+          src={currentUser?.image || avatar.src}
+          alt={currentUser?.name || "User Avatar"}
+          className="w-full h-full object-cover"
+        />
+      </button>
+      <span>
+        <h4 className="truncate capitalize">mohammed nazim</h4>
+        <p className="text-xs text-gray-400">View Profile</p>
+      </span>
+    </div>
   );
 };
 
